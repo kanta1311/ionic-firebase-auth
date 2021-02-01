809575ee43d13e8c87d2690ce66102d1d388c6c7
Arvind Kant <kanta1311@gmail.com>
2021-02-01 11:12:15 +0530

    added firebase plugin

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FirebaseAuthentication } from '@ionic-native/firebase-authentication/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private firebaseAuthentication: FirebaseAuthentication) {}
+
+  emailLogin(){
+    this.firebaseAuthentication.createUserWithEmailAndPassword('test@gmail.com', '123')
+    .then((res: any) => console.log(res))
+    .catch((error: any) => console.error(error));
+  }
 
 }
